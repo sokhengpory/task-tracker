7966f91e06ced6b18f3ee5c225f167930ccdae47
pnek <porysokheng@gmail.com>
2021-05-15 19:11:18 +0700

    Toggle the reminder function on task

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,7 +2,10 @@ import { FaTimes } from 'react-icons/fa';
 
 const Task = ({ task, onDelete, onToggle }) => {
   return (
-    <div className="task" onDoubleClick={() => onToggle(task.id)}>
+    <div
+      className={task.reminder ? 'task reminder' : 'task'}
+      onDoubleClick={() => onToggle(task.id)}
+    >
       <h3>
         {task.text}{' '}
         <FaTimes style={{ color: 'red' }} onClick={() => onDelete(task.id)} />
